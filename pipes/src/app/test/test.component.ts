5c7529c359cc2074cad4fe6b3dec998799b29ee3
madhav singh <madhav201singh@gmail.com>
2023-08-31 11:26:40 +0530

    update pipes

--- pipes/src/app/test/test.component.ts
+++ pipes/src/app/test/test.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
   presentDate = new Date();
+  // Build-in-Pipes 
+  // Build-in-Pipes | Async
+  timeChange!: Observable<string>;
+  // Build-in-Pipes | Currency Pipe
+  price:number =20000
+  ngOnInit() {
+    this.timeChange = new Observable<string>((observer: Observer<string>) => {
+      setInterval(() => observer.next(new Date().toString()), 1000);
+    });
+  }
 }
