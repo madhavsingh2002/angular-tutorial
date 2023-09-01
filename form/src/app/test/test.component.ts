b7c150ed10dfc5110cce6da8a027fba422fc1b65
madhav singh <madhav201singh@gmail.com>
2023-09-01 10:30:07 +0530

    update validation

--- form/src/app/test/test.component.ts
+++ form/src/app/test/test.component.ts
@@ -1,5 +1,5 @@
 import { Component ,OnInit} from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl ,Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
@@ -7,7 +7,7 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  userName: string="";
+  /*  userName: string="";
   formdata!: FormGroup;
   
   ngOnInit(): void{
@@ -19,6 +19,41 @@ export class TestComponent implements OnInit {
   onClickSubmit() {
     this.userName = this.formdata.value.userName;
   }
+  */
+
+/*  Create FormGroup
+  requiredForm!: FormGroup;
+  constructor(private fb: FormBuilder){
+    this.myForm();
+  }
+  // Create required Field validator for name
+  myForm(){
+    this.requiredForm = this.fb.group({
+      name:['',Validators.required]
+    })
+  }
+  ngOnInit(): void {
+    
+  }
+  */
+
+  /* PATTERN VALIDATOR*/ 
+  requiredForm!: FormGroup;
+   constructor(private fb: FormBuilder) {
+      this.myForm();
+   }
+
+   myForm() {
+      this.requiredForm = this.fb.group({
+      email: ['', [Validators.required, 
+         Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")] ]
+      });
+   }
+
+   ngOnInit()
+   {
+
+   }
 //   onClickSubmit(result: { username: string; }) {
 //     console.log("You have entered : " + result.username); 
 //  }
